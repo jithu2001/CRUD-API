c06e21aee7c367184af87d36b83d6b58b0237eff
Jithu2001 <jithu.george@perleybrook.com>
2024-12-01 19:41:51 +0530

    Create a Book management with CRUD API

---
+++ cmd/main/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"jithu2001/CRUD-API/pkg/routes"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
